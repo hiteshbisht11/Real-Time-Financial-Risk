4ab3c5cc56767e6e669f45a0edce74386437130c
hiteshbisht <hitesh.bisht@zzazz.ai>
2026-02-19 00:40:02 +0530

    build repo structure

---
+++ src/datasets/adapters/ieee_adapter.py
@@ -0,0 +1,3 @@
+# Modeling + Feature Complexity Focused Adapter for the IEEE Dataset
+# This adapter is designed to transform the IEEE dataset into a format suitable for complex modeling and feature engineering
+#  using it to build the full end to end platform
---
+++ src/datasets/adapters/paysim_adapter.py
@@ -0,0 +1,3 @@
+# System + Streaming Focused Adapter for the PaySim Dataset
+# This adapter is designed to transform the PaySim dataset into a format suitable for real-time streaming
+#  using it to build the full end to end platform
